94dad969b6d3a2368d6317748d300a0a1fcbb0c6
Coooder-X <1104143015@qq.com>
2022-02-22 00:10:45 +0800

    fear: 创建、加入room逻辑

--- src/page/Menu/index.tsx
+++ src/page/Menu/index.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
-import{ io } from 'socket.io-client';
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 import axios from "axios";
 
 const socket = io('http://localhost:3020/room');
+socket.on('broadcast', (data: any) => {
+    console.log('rec 广播', data);
+})
 
 const Menu: React.FC<{}> = () => {
 
     const [roomId, setRoomId] = useState('');
+    const [newRoomId, setNewRoomId] = useState('');
+
+    useEffect(() => {
+        socket.emit('connect-server', { data: 'new client' });
+    }, []);
 
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
@@ -16,20 +24,39 @@ const Menu: React.FC<{}> = () => {
         setRoomId(input);
     }
 
+    const handleInputNew = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setNewRoomId(e.target.value);
+    }
+
     const handleJoinRoom = () => {
         console.log('join');
         console.log(socket);
-        socket.emit('event', { data: '1234234243' }, (data: any) => console.log(data));
+        if (!roomId || roomId === '') {
+            alert('请输入房间号')
+            return;
+        }
+        socket.emit('joinRoom', { roomId }, (data: any) => {
+            if (!data) {
+                alert('房间可能已存在！')
+            } else if (data.hasOwnProperty('msg')) {
+                alert(data.msg);
+            }
+            console.log(data);
+        });
     }
 
     const handleCreateRoom = () => {
         console.log('create');
-        socket.emit('message', { data: 'this is data', value: 123 }, (data: any) => console.log(data));
+        socket.emit('createRoom', { roomName: newRoomId }, (data: any) => console.log(data));
     }
 
     return <>
         <div>
-            <button onClick={handleCreateRoom}> {'创建房间'} </button>
+            <div>
+                <input style={{ display: 'inline-block' }} value={newRoomId} onChange={handleInputNew} />
+                <button onClick={handleCreateRoom}> {'创建房间'} </button>
+            </div>
             <div>
                 <input style={{ display: 'inline-block' }} value={roomId} onChange={handleInput} />
                 <button style={{ display: 'inline-block' }} onClick={handleJoinRoom}> {'加入房间'} </button>
